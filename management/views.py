662b257d7de06d2e750df6686aad10f7f81da48a
LAKSHIT SHARMA <slakshit@yahoo.com>
2021-08-29 05:59:20 +0530

    Models created, homepage is also completed

--- management/views.py
+++ management/views.py
@@ -1,10 +1,37 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.core.mail import send_mail
+from . import models,forms
 
 # Create your views here.
 def home(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
     # return HttpResponse("this is index")
-    return render(request,'base.html')
+    return render(request,'index.html')
+
+#---------------------------------------------------------------------------------
+#------------------------ ABOUT US AND CONTACT US VIEWS START ------------------------------
+#---------------------------------------------------------------------------------
+def about_us(request):
+    return render(request,'hospital/aboutus.html')
+
+def contact_us(request):
+    sub = forms.ContactusForm()
+    if request.method == 'POST':
+        sub = forms.ContactusForm(request.POST)
+        if sub.is_valid():
+            email = sub.cleaned_data['Email']
+            name=sub.cleaned_data['Name']
+            message = sub.cleaned_data['Message']
+            send_mail(str(name)+' || '+str(email),message,settings.EMAIL_HOST_USER, settings.EMAIL_RECEIVING_USER, fail_silently = False)
+            return render(request, 'hospital/contactussuccess.html')
+    return render(request, 'contact_us.html', {'form':sub})
+
+
+#---------------------------------------------------------------------------------
+#------------------------ ABOUT US AND CONTACT US END ------------------------------
+#---------------------------------------------------------------------------------
+
